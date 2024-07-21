6483ef0c821c6b4c8d09260507cb852952b8294d
Aubrey Clark <zane.a.s.clark@gmail.com>
2024-07-21 16:20:19 -0700

    upgraded which key bindings

--- init.lua
+++ init.lua
@@ -110,7 +110,24 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  {
+    'folke/which-key.nvim',
+    opts = {},
+    keys = {
+      { "<leader>c", group = "[C]ode" },
+      -- { "<leader>c_", hidden = true },
+      { "<leader>d", group = "[D]ocument" },
+      -- { "<leader>d_", hidden = true },
+      { "<leader>g", group = "[G]it" },
+      -- { "<leader>g_", hidden = true },
+      { "<leader>r", group = "[R]ename" },
+      -- { "<leader>r_", hidden = true },
+      { "<leader>s", group = "[S]earch" },
+      -- { "<leader>s_", hidden = true },
+      { "<leader>w", group = "[W]orkspace" }
+      -- { "<leader>w_", hidden = true },
+    },
+  },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -297,10 +314,6 @@ require('lazy').setup({
     opts = {
       autosave_ignore_buftypes = { 'terminal' }, -- this is bc, on windows at least, terminal breaks when reloaded
     }
-  },
-  {
-    "kylechui/nvim-surround",
-    event = "VeryLazy",
   },
   {
     'stevearc/aerial.nvim',
@@ -541,17 +554,6 @@ local on_attach = function(_, bufnr)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
 end
-
--- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  -- ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
